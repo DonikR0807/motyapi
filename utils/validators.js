4e4d4747089feedea8381f290caf64e83d000259
Donier Rakhimov <Rdonik0807@yandex.ru>
2023-09-08 23:00:11 +0300

    added joi validator

--- utils/validators.js
+++ utils/validators.js
@@ -8,6 +8,13 @@ const linkValidator = (value, helpers) => {
   return value;
 };
 
+const categoryValidator = (value, helpers) => {
+  if (value !== 'saved' && value !== 'planned') {
+    return helpers.error('incorrect category');
+  }
+  return value;
+};
+
 const loginValidator = {
   body: Joi.object().keys({
     email: Joi.string().required().email(),
@@ -30,32 +37,69 @@ const updateProfileValidator = {
   }),
 };
 
-const saveMovieValidator = {
-  body: Joi.object().keys({
-    country: Joi.string().required(),
-    director: Joi.string().required(),
-    duration: Joi.string().required(),
-    year: Joi.string().required(),
-    description: Joi.string().required(),
-    image: Joi.string().required().custom(linkValidator),
-    trailerLink: Joi.string().required().custom(linkValidator),
-    thumbnail: Joi.string().required().custom(linkValidator),
-    movieId: Joi.number().required(),
-    nameRU: Joi.string().required(),
-    nameEN: Joi.string().required(),
+const getCommentsValidator = {
+  params: Joi.object().keys({
+    animeId: Joi.number().required(),
   }),
 };
 
-const deleteMovieValidator = {
-  params: Joi.object().keys({
-    movieId: Joi.string().required().hex().length(24),
+const createCommentValidator = {
+  body: Joi.object().keys({
+    createdAt: Joi.date().required(),
+    text: Joi.string().required(),
+    animeId: Joi.number().required(),
   }),
 };
+
+const saveAnimeValidator = {
+  body: Joi.object().keys({
+    animeId: Joi.number().required(),
+    category: Joi.string().required().custom(categoryValidator),
+    genres: Joi.array().required().items(Joi.string().required()),
+    status: Joi.object().required().keys({
+      string: Joi.string().required(),
+    }),
+    names: Joi.object().required().keys({
+      ru: Joi.string().required(),
+    }),
+    image: Joi.string().required().custom(linkValidator),
+  }),
+};
+
+const deleteAnimeValidator = {
+  params: Joi.object().keys({
+    animeId: Joi.string().required(),
+  }),
+};
+
+// const saveMovieValidator = {
+//   body: Joi.object().keys({
+//     country: Joi.string().required(),
+//     director: Joi.string().required(),
+//     duration: Joi.string().required(),
+//     year: Joi.string().required(),
+//     description: Joi.string().required(),
+//     image: Joi.string().required().custom(linkValidator),
+//     trailerLink: Joi.string().required().custom(linkValidator),
+//     thumbnail: Joi.string().required().custom(linkValidator),
+//     movieId: Joi.number().required(),
+//     nameRU: Joi.string().required(),
+//     nameEN: Joi.string().required(),
+//   }),
+// };
+
+// const deleteMovieValidator = {
+//   params: Joi.object().keys({
+//     movieId: Joi.string().required().hex().length(24),
+//   }),
+// };
 
 module.exports = {
   loginValidator,
   registerValidator,
   updateProfileValidator,
-  saveMovieValidator,
-  deleteMovieValidator,
+  getCommentsValidator,
+  createCommentValidator,
+  saveAnimeValidator,
+  deleteAnimeValidator,
 };
