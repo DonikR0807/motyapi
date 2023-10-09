8b5065bbab752f3b1ed792c6ba71ece1d7f4321a
DonikR0807 <donikclonik2001@mail.ru>
2023-10-10 01:55:11 +0300

    added sorting,filtration,search

--- src/controllers/anime.js
+++ src/controllers/anime.js
@@ -1,39 +1,60 @@
-import Anime from "../models/Anime.js";
+import Anime from '../models/Anime.js';
 
 const getAnimes = async (req, res, next) => {
   try {
-    const { page = 1, limit = 5 } = req.query;
-    const parsedPage = parseInt(page);
-    const parsedLimit = parseInt(limit);
-  
-    const startIndex = (parsedPage - 1) * parsedLimit;
-    const endIndex = parsedPage * parsedLimit;
-  
-    const result = {};
-    
-    const allAnimes = await Anime.find({});
-  
-    if (startIndex > 0) {
-      result.previous = {
-        page: parsedPage - 1,
-        limit: parsedLimit,
-      };
-    }
-  
-    if (endIndex < allAnimes.length) {
-      result.next = {
-        page: parsedPage + 1,
-        limit: parsedLimit,
-      };
+    const page = parseInt(req.query.page) - 1 || 0;
+    const limit = parseInt(req.query.limit) || 5;
+    const search = req.query.search || '';
+    let sort = req.query.sort || 'names.ru';
+    let genre = req.query.genre || 'All';
+
+    if (genre === 'All') {
+      genre = [...genre];
+    } else {
+      genre = genre.split(',');
     }
 
-    result.list = allAnimes.slice(startIndex, endIndex);
-    res.send(result);
+    if (sort === 'names.ru') {
+      sort = [sort];
+    } else {
+      sort = sort.split(',');
+    }
+    let sortBy = {};
+    if (sort[1]) {
+      sortBy[sort[0]] = sort[1];
+    } else {
+      sortBy[sort[0]] = 'asc';
+    }
+
+    const animes = await Anime.find({
+      'names.ru': {
+        $regex: search,
+        $options: 'i',
+      },
+    })
+      .where('genre')
+      .in([...genre])
+      .sort(sortBy)
+      .skip(page * limit)
+      .limit(limit);
+
+    const totalAnimes = await Anime.countDocuments({
+      genre: { $in: [...genre] },
+      'names.ru': {
+        $regex: search,
+        $options: 'i',
+      },
+    });
+
+    res.send({
+      list: animes,
+      total: totalAnimes
+    })
   } catch (err) {
     next(err);
   }
-}
+};
 
 export default {
   getAnimes,
-}
+};
